8c6cf29fbf04595d56465191424a0301510af261
ts3m <matt.tse.cs@gmail.com>
2012-07-28 13:39:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniDuke::Application.config.secret_token = '2ebd82701317cba1b1a55ef374035ce6bf5aee9c215024fc1c492a281a756ce7580e9de496a57bb6f4928a93a17c06f95ea3faccd598c8af0b57f9f2f829712c'
