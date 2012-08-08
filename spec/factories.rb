9ff48c5d046f6cb2f9b8d345550f1d73721902cf
ts3m <matt.tse.cs@gmail.com>
2012-08-08 10:17:31 -0700

    added section spec, added FactoryGirl to remove NDianness in testing

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
 
   factory :course do
-    name "Computer Science 100"
-    description "A really great class"
+    name Faker::Lorem.sentence
+    description Faker::Lorem.paragraphs
     credits 1
   end
 
   factory :section do
-    suffix "01L"
-    section_type "LEC"
-    location "LSRC B101"
+    suffix  Faker::Lorem.words(1)[0]
+    section_type Faker::Lorem.words(1)[0]
+    location  Faker::Address.secondary_address
     enrollment 15
     capacity 100
     waitlist_enrollment 0
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -16,11 +16,36 @@ describe Section do
 
   #skipped all the useless validations of all the fields, they should all be there when it is created
 
-  describe "pointer to course should work" do
+  describe "pointer from section to course should work when link is created from section" do
     before do
       @course = FactoryGirl.create(:course)
+      @section = FactoryGirl.create(:section)
+      @section.course = @course
+      @section.save
     end
+
+    its(:course) {should == @course}
+    
+
+    it "and section should be pointed at by the course" do
+      @course.sections.should include(@section)
+    end
+ 
+
   end
 
+  describe "pointer from section to course should work when link is created from course" do
+    before do
+      @course = FactoryGirl.create(:course)
+      @section = FactoryGirl.create(:section)
+      @course.sections<<@section
+      @course.save
+    end
+
+    its(:course) { should == @course}
+    it "and pointer from course to section should work as well" do
+      @course.sections.should include(@section)
+    end
+  end
 end
 
