c12922edad6c6b9cd48119c481ce4ea95e2eae34
ts3m <matt.tse.cs@gmail.com>
2012-08-15 13:13:54 -0400

    commented out broken parts of the views, they shuld work now, tests still broken

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -3,21 +3,15 @@ require 'spec_helper'
 describe Course do
 
   before do
-    @course = Course.new(name:"CompSci 100", description:"a description", credits:1)
+    @course = Course.new(name:"CompSci 100")
   end
 
   subject { @course }
   it { should respond_to(:name)}
-  it { should respond_to(:description)}
-  it { should respond_to(:credits)}
-  it { should respond_to(:synopsis)}
-  it { should respond_to(:seminar)}
   it { should respond_to(:session_id)}
   it {should respond_to(:prerequisites)}
   it { should respond_to(:sections)}
   it { should respond_to(:instructors)}
-  it { should respond_to(:modes_of_inquiry)}
-  it { should respond_to(:areas_of_knowledge)}
   it { should respond_to(:course_numberings)}
   it { should respond_to(:subjects)}
   it { should be_valid}
