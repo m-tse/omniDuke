64c81d11f10cb2a4cd09734ba6afb22f2336ecce
Matthew Tse <matt.s.tse@gmail.com>
2012-12-09 14:19:11 -0500

    Fixed bug where new users did not have a default session, they now have a default session.  Also removed deprecated _null.html.erb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  #a user must always have a current session he/she is browsing to/from
+  validates :session, presence:true
+
+  before_validation do
+    if(self.session==nil)
+      self.session=Session.find_by_name("fall2012"); #eventually change this to a default global constant
+    end
+  end
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
