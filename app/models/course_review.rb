74fb63d2ef2bc583fad0195b57b67349ad6116b6
Matthew Tse <matt.s.tse@gmail.com>
2012-12-27 12:34:02 -0500

    further styling for reviews

--- app/models/course_review.rb
+++ app/models/course_review.rb
@@ -1,6 +1,6 @@
 class CourseReview < ActiveRecord::Base
   attr_accessible :usefulness, :stimulating, :content_quality, :homework_difficulty, 
-  :lab_difficulty, :midterm_difficulty, :final_difficulty, :course_meta_id
+  :lab_difficulty, :midterm_difficulty, :final_difficulty, :course_meta_id, :out_of_class_work_hours
   belongs_to :course_meta
   validates :course_meta_id, presence:true
   after_save{
