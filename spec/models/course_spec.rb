e8a330c4390bdae4d307861aaec8fb2a6b1aa2c6
Matthew Tse <matt.s.tse@gmail.com>
2013-01-05 01:44:05 -0500

    added small test

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,8 +4,20 @@ describe Course do
 
   before do
     @course = Course.new(name:"CompSci 100")
+
   end
 
   subject { @course }
+  it {should be_valid}
 
+  describe "section links should function correctly" do
+    before do
+      @section = FactoryGirl.create(:section)
+      @course.sections<<@section
+      @section.save
+      @course.save
+    end
+    its(:sections) {should include(@section)}
+
+  end
 end
