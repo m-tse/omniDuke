6e8e1f971d44f863302e3b2916761a9bce5e36ad
Matthew Tse <matt.s.tse@gmail.com>
2013-01-06 19:42:19 -0500

    fixed minor view bug in course_overview, made instructor_review model self update its average score after each change to the database

--- app/models/instructor_review.rb
+++ app/models/instructor_review.rb
@@ -3,7 +3,36 @@ class InstructorReview < ActiveRecord::Base
   belongs_to :instructor
 
   validates :instructor_id, presence:true
+
+  before_save{
+    if self.author==nil||self.author==""
+      self.author="Anonymous"
+    end
+    update_average_scores
+  }
+
   after_save{
   	self.instructor.save
   }
+
+
+
+  def update_average_scores
+    courseQualitySum = 0.0
+    courseQualityCounter = 0.0
+    courseCriteria = [self.helpfulness, self.accessibility, self.clarity, self.fairness]
+    for rating in courseCriteria
+      if rating!=nil&&rating!=0
+        courseQualityCounter+=1
+        courseQualitySum+=rating
+      end
+    end
+    if courseQualityCounter == 0.0
+      courseQualityCounter+=1.0
+    end
+    self.average_quality=(courseQualitySum/courseQualityCounter).to_f
+    
+  end
 end
+
+
